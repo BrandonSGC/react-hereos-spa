4cf544cccdcf14e021b93ed6119bed0accd295e3
BrandonSGC <brangmz20@gmail.com>
2024-05-30 13:58:14 -0600

    fix - Heroes list responsive

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -6,7 +6,7 @@ export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHerosByPublisher(publisher), [publisher]);
 
   return (
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       {heroes.map((hero) => (
         <div key={hero.id} className="col">
           <HeroCard {...hero} />
